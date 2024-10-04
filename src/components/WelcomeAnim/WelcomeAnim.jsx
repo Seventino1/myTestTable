bf0673a6d2a5fec9c1cf27928591704cc2aad4fb
VolkovI <Joker_077@mail.ru>
2024-10-04 14:14:08 +0300

    WelcomeAnim

--- src/components/WelcomeAnim/WelcomeAnim.jsx
+++ src/components/WelcomeAnim/WelcomeAnim.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import logoPath from "../../assets/logo.svg";
+import "./style.css";
 
 export const WelcomeAnim = () => {
+    const [hasAnimScreen, setAnimScreen] = useState(true);
+
+    useEffect(() => {
+        sessionStorage.setItem("hasAnim", "false");
+    setTimeout(()=> {
+        setAnimScreen(false);
+    }, 1000);
+    }, []);
+
     return (
-        <div className="welcome-anim">
-            welcome-anim
+        <div className={"welcome-anim" + (hasAnimScreen ? "" : " welcome-anim_hidden")}>
+            <div className="">
+                <img src={logoPath} alt="keystone" />
+                <div className="welcome-animation__title">
+                    отражение<br /> успеха
+                </div>
+            </div>
         </div>
     );
 };
+
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { WelcomeAnim } from "../components/WelcomeAnim/WelcomeAnim";
+import { SearchComponent } from "../components/SearchComplonent/SearchComponent"
 
 export const MainPage = () => {
     return (
         <div className="main-page">
             <WelcomeAnim />
+            <div className="page-title"> Введите данные для уточнения вашего места за столом</div>
+            <SearchComponent />
         </div>
     );
 };
