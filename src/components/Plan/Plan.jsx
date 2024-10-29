25e1235b88c30c4abbd9aea437e79de5c74d6780
VolkovI <Joker_077@mail.ru>
2024-10-29 09:54:44 +0300

    the end

--- src/components/Plan/Plan.jsx
+++ src/components/Plan/Plan.jsx
@@ -1,7 +1,79 @@
-export const Plan = () => {
-    return (
-        <div className="plan">
-            plan
+import { CircleTable } from "./CircleTable";
+import { CommonTable } from "./CommonTable";
+import "./style.css";
+
+export const Plan = ({ table, side }) => {
+  return (
+    <div className="plan">
+      <div className="scena">Сцена</div>
+      <div className="plan__area-1">
+        <div className="plan__side">
+          <div className="plan__column">
+            <CommonTable activeNumber={table} side={side} number={40} />
+            <CommonTable activeNumber={table} side={side} number={39} />
+            <CommonTable activeNumber={table} side={side} number={38} />
+            <CommonTable activeNumber={table} side={side} number={37} />
+            <CommonTable activeNumber={table} side={side} number={36} />
+            <CommonTable activeNumber={table} side={side} number={35} />
+            <CommonTable activeNumber={table} side={side} number={34} />
+          </div>
+          <div className="plan__column">
+            <CommonTable activeNumber={table} side={side} number={33} />
+            <CommonTable activeNumber={table} side={side} number={32} />
+            <CommonTable activeNumber={table} side={side} number={31} />
+            <CommonTable activeNumber={table} side={side} number={30} />
+            <CommonTable activeNumber={table} side={side} number={29} />
+            <CommonTable activeNumber={table} side={side} number={28} />
+            <CommonTable activeNumber={table} side={side} number={27} />
+          </div>
+          <div className="plan__column">
+            <CommonTable activeNumber={table} side={side} number={26} />
+            <CommonTable activeNumber={table} side={side} number={25} />
+            <CommonTable activeNumber={table} side={side} number={24} />
+            <CommonTable activeNumber={table} side={side} number={23} />
+            <CommonTable activeNumber={table} side={side} number={22} />
+            <CommonTable activeNumber={table} side={side} number={21} />
+          </div>
         </div>
-    );
+        <div className="">1 этаж</div>
+        <div className="plan__side">
+          <div className="plan__column">
+            <CommonTable activeNumber={table} side={side} number={20} />
+            <CommonTable activeNumber={table} side={side} number={19} />
+            <CommonTable activeNumber={table} side={side} number={18} />
+            <CommonTable activeNumber={table} side={side} number={17} />
+            <CommonTable activeNumber={table} side={side} number={16} />
+            <CommonTable activeNumber={table} side={side} number={15} />
+          </div>
+          <div className="plan__column">
+            <CommonTable activeNumber={table} side={side} number={14} />
+            <CommonTable activeNumber={table} side={side} number={13} />
+            <CommonTable activeNumber={table} side={side} number={12} />
+            <CommonTable activeNumber={table} side={side} number={11} />
+            <CommonTable activeNumber={table} side={side} number={10} />
+            <CommonTable activeNumber={table} side={side} number={9} />
+            <CommonTable activeNumber={table} side={side} number={8} />
+          </div>
+          <div className="plan__column">
+            <CommonTable activeNumber={table} side={side} number={7} />
+            <CommonTable activeNumber={table} side={side} number={6} />
+            <CommonTable activeNumber={table} side={side} number={5} />
+            <CommonTable activeNumber={table} side={side} number={4} />
+            <CommonTable activeNumber={table} side={side} number={3} />
+            <CommonTable activeNumber={table} side={side} number={2} />
+            <CommonTable activeNumber={table} side={side} number={1} />
+          </div>
+        </div>
+      </div>
+      <div className="plan__area-2">
+        <div className="">2 этаж</div>
+        <div className="plan__area-2-inner">
+          <CircleTable activeNumber={table} number={44} />
+          <CircleTable activeNumber={table} number={43} />
+          <CircleTable activeNumber={table} number={42} />
+          <CircleTable activeNumber={table} number={41} />
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/SearchComplonent/SearchComponent.jsx
+++ src/components/SearchComplonent/SearchComponent.jsx
@@ -1,8 +1,51 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import lupaPath from "../../assets/lupa.svg";
+import xPath from "../../assets/x.svg";
+import "./style.css";
+import Highlighter from "react-highlight-words";
+import { USERS } from "../../users";
 
 export const SearchComponent = () => {
+
+    const [value, setValue] = useState("");
+    const filteredUsers = USERS.filter(({ name })=>
+    name.toLowerCase().includes(value.toLowerCase()));
+
     return (
         <div className="search-component">
-            search-component
-        </div>
-    );
+            <div className="search-component__input-wrapper">
+                <img src={lupaPath} alt="lupa" className="icon" />
+                <input 
+                placeholder="Familia Name"
+                value={value}
+                onChange={(e)=> setValue(e.target.value)}
+                className="search-component__input"
+                />
+                <img src={xPath} alt="x" className="icon" onClick={()=> setValue("")} />
+            </div>
+            {value === "" && (
+                <div className="search-component__info">
+                    Если вы с гостем пришли, вы нахлебник, введите его имя
+                </div>
+            )}
+            {value !== "" && (
+                 <ul className="search-component__list">
+          {filteredUsers.map(({ name, id }) => (
+            <Link key={id} to={`/plan?id=${id}`} className="search-component__list-item">
+              <li>
+                <Highlighter searchWords={[value]} autoEscape={true} textToHighlight={name} />
+              </li>
+            </Link>
+          ))}
+          {filteredUsers.length === 0 && (
+            <div className="search-component__empty-state">
+              Гостей с таким именем не найдено <br />
+              Проверьте введенные данные
+            </div>
+          )}
+        </ul>
+      )}
+    </div>
+  );
 };
